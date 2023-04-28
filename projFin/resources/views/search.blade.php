c96815b611d7d08e010b16876e0072ab59edddbd
dr SAlma <drSalma@gmail.com>
2023-04-28 22:55:01 +0200

    remove folder

--- projFin/resources/views/search.blade.php
+++
@@ -1,8 +0,0 @@
-<!-- <form action="" class="d-flex mr-3 mb-1">
-    <div class="form-group mb-0 mr-1">
-        
-<input type="text" name="nom" class="form-control">
-    </div>
-<button type="submit" class="btn btn-info"><i class="fa fa-search" aria-hidden="true"></i></button>
-
-</form> -->
