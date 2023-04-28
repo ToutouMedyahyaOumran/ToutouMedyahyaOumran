e055a295362dab2ed2862fd9b3f76de6cd831352
youssef <youssoufbeyba72@gmail.com>
2023-04-28 03:43:42 +0200

    first commit of yusuf

---
+++ projFin/app/Http/Livewire/Search.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use App\Models\Planning;
+use Livewire\Component;
+
+class Search extends Component
+{
+    
+
+    public function render()
+    {
+        
+    }
+}
+
+?>
